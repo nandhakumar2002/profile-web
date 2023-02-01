03e1a7e3af9ce583afd84f5af1789204b6592fa7
Nandha22 <nandhakumargunasekar22@gmail.com>
2023-02-01 05:58:59 +0530

    intial changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My Portfolio</h1>
       </header>
+      <section className="about-section">
+        <h2>About Me</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
+          euismod odio, gravida pellentesque urna varius vitae.
+        </p>
+      </section>
+      <section className="projects-section">
+        <h2>Projects</h2>
+        <ul>
+          <li>
+            <h3>Project 1</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+              viverra euismod odio, gravida pellentesque urna varius vitae.
+            </p>
+          </li>
+          <li>
+            <h3>Project 2</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+              viverra euismod odio, gravida pellentesque urna varius vitae.
+            </p>
+          </li>
+        </ul>
+      </section>
+      <footer className="footer">
+        <p>Copyright © {new Date().getFullYear()}</p>
+      </footer>
     </div>
   );
 }
