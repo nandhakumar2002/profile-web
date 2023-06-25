bc01e67f99c7debfec588842aecfa99cc18fe0ec
Nandha22 <nandhakumargunasekar22@gmail.com>
2023-06-25 14:16:29 +0530

    Updates

---
+++ precache-manifest.8553816a1c12bc98dde1e7d8abdf4d13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "193ae50da61293b4f767",
+    "url": "/profile-web/static/css/main.558b458d.chunk.css"
+  },
+  {
+    "revision": "193ae50da61293b4f767",
+    "url": "/profile-web/static/js/main.193ae50d.chunk.js"
+  },
+  {
+    "revision": "2511f89111de37f4dae3",
+    "url": "/profile-web/static/js/1.2511f891.chunk.js"
+  },
+  {
+    "revision": "b333792729ca0435236a",
+    "url": "/profile-web/static/js/2.b3337927.chunk.js"
+  },
+  {
+    "revision": "31186b434a905330c0c3",
+    "url": "/profile-web/static/js/runtime~main.31186b43.js"
+  },
+  {
+    "revision": "42471b183f176e056ab18d17407e6a00",
+    "url": "/profile-web/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/profile-web/precache-manifest.2d95072600b079e544942dbfed49a3b9.js"
+  "/profile-web/precache-manifest.8553816a1c12bc98dde1e7d8abdf4d13.js"
 );
 
 workbox.clientsClaim();
