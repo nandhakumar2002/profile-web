fb0707147bf02e958734bfcb6e5c2d098e90d493
Nandha22 <nandhakumargunasekar22@gmail.com>
2023-06-25 19:55:02 +0530

    bug fix

--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -19,7 +19,7 @@ const NavBar = (props) => {
 										: "nav-item"
 								}
 							>
-								<Link to="/">Home</Link>
+								<Link to="/profile-web/">Home</Link>
 							</li>
 							<li
 								className={
